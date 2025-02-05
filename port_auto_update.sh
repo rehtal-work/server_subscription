7a7f95c9950c2b16a6291db5d80a2317701365cd
root <root@hot-ducks-2.localdomain>
2025-02-05 06:35:52 -0500

    bandwagon_LA auto update: 2025-02-05 19:35:52

--- port_auto_update.sh
+++ port_auto_update.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 set -e  # 脚本执行出错时自动退出
 
+if [ $# -lt 1 ]; then
+  echo "No host name"
+  exit 1
+fi
+
 # 配置项
 CONFIG_FILE="/usr/local/etc/v2ray/config.json"
 RANDOM_PORT_RANGE="15300-20000"
-HOST_NAME="bandwagon_amsterdam"
+HOST_NAME=$1
 
 # 生成随机端口（15300~20000）
 NEW_PORT=$(shuf -i $RANDOM_PORT_RANGE -n 1)
